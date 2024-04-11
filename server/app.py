51a5d6bc899d8de0ea64f2530d109d868d24dc9d
+ <+>
2024-04-11 19:40:31 +0300

    Completed Assignmnet

--- server/app.py
+++ server/app.py
@@ -28,16 +28,41 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+            else:
+                return {'message': 'User not found'}, 404
+        return {'message': 'Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        if not username or not password:
+            return {'error': 'Username or password missing'}, 400
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/checksession', endpoint='checksession')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
